3913dc4c3ca1e24eda730e4905c7ed2a3521e793
Thijs Zumbrink <tazaranie@gmail.com>
2017-08-09 15:16:55 +0200

    Add rudimentary Minimax integration

--- app/public/index.php
+++ app/public/index.php
@@ -1,8 +1,10 @@
 <?php
 
+use lucidtaz\minimax\engine\Engine;
 use lucidtaz\reversi\core\Application;
 use lucidtaz\reversi\gamelogic\Board;
 use lucidtaz\reversi\gamelogic\GameState;
+use Symfony\Component\HttpFoundation\Request;
 
 require_once __DIR__ . '/../../vendor/autoload.php';
 
@@ -26,16 +28,29 @@ $app->view(function (GameState $gameState) use ($app) {
             'width' => Board::SIZE_X,
             'height' => Board::SIZE_Y,
         ],
+        'analytics' => $app->session->get('analytics'),
     ]);
 });
 
-$app->get('/', function () use ($app, $gameState) {
+$app->get('/', function () use ($gameState) {
     return $gameState;
 });
 
 $app->post('/move/{x}/{y}', function (int $x, int $y) use ($app, $gameState) {
     $gameState->makeMove($y, $x);
     $app->session->set('gamestate', serialize($gameState));
+    return $app->redirect('/');
+})
+->assert('x', '\d+')
+->assert('y', '\d+');
+
+$app->post('/move/auto', function (Request $request) use ($app, $gameState) {
+    $lookAhead = (int) $request->get('lookAhead');
+    $minimax = new Engine($gameState->getNextPlayer(), $lookAhead);
+
+    $newGameState = $minimax->decide($gameState);
+    $app->session->set('gamestate', serialize($newGameState));
+    $app->session->set('analytics', $minimax->getAnalytics());
     return $app->redirect('/');
 });
 
